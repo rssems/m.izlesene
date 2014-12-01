aeed91d7ad006773de5a7e726ebc6cbc510b4c4f
Rıza Selçuk Saydam <rss.ems@gmail.com>
2014-12-01 14:53:14 +0200

    tv

--- video_tv.php
+++ video_tv.php
@@ -36,102 +36,99 @@
 					<div class="tooltip_up"></div>
 					<div class="clear"></div>
 					<div class="tooltip_title">VİDEOYU TV'YE YANSIT</div>
+					<div class="mini-fix"></div>
 					<div class="clear"></div>
 					<div class="tooltip_content">
 
+						<div class="tvreflect_icons" id="tvreflect_icons">
+							<div class="tvreflect_input"><input type="number" placeholder=" ***** "></div>
+							<span class="tvreflect_sprite tvreflect_icon_phone"></span>
+							<span class="tvreflect_sprite tvreflect_icon_play"></span>
+							<span class="tvreflect_sprite tvreflect_icon_tv"></span>
+							<span class="tvreflect_sprite tvreflect_icon_success"></span>
+						</div><!-- tvreflect_icons -->
+						<p class="tvreflect_info tvreflect_info1" id="p1">Büyük ekran keyfi ile<br>video izlemek için Smart TV’nizden<br>‘<a href="http://tv.izlesene.com">http://tv.izlesene.com</a>’u açınız.</p><!-- tvreflect_info -->
+						<p class="tvreflect_info tvreflect_info2 hide" id="p2">Smart TV’nizden alacağınız<br>kodu buraya girin.</p><!-- tvreflect_info -->
+						<p class="tvreflect_info tvreflect_info3 hide" id="p3"><i>Tebrikler!</i> Doğrulama başarılı.<br>Smart TV’de büyük ekran keyfine başla.</p><!-- tvreflect_info -->
+						<p class="tvreflect_info tvreflect_info4 hide" id="p4"><i>Yanlış kod girdiniz, tekrar deneyiniz.</i> Smart TV’nizde beliren kodu giriniz.</p><!-- tvreflect_info -->
 
-					<div class="tvreflect_icons" id="tvreflect_icons">
-						<div class="tvreflect_input"><input type="number" placeholder=" ***** "></div>
-						<span class="tvreflect_sprite tvreflect_icon_phone"></span>
-						<span class="tvreflect_sprite tvreflect_icon_play"></span>
-						<span class="tvreflect_sprite tvreflect_icon_tv"></span>
-						<span class="tvreflect_sprite tvreflect_icon_success"></span>
-					</div><!-- tvreflect_icons -->
-					<p class="tvreflect_info tvreflect_info1" id="p1">Büyük ekran keyfi ile<br>video izlemek için Smart TV’nizden<br>‘<a href="http://tv.izlesene.com">http://tv.izlesene.com</a>’u açınız.</p><!-- tvreflect_info -->
-					<p class="tvreflect_info tvreflect_info2 hide" id="p2">Smart TV’nizden alacağınız<br>kodu buraya girin.</p><!-- tvreflect_info -->
-					<p class="tvreflect_info tvreflect_info3 hide" id="p3"><i>Tebrikler!</i> Doğrulama başarılı.<br>Smart TV’de büyük ekran keyfine başla.</p><!-- tvreflect_info -->
-					<p class="tvreflect_info tvreflect_info4 hide" id="p4"><i>Yanlış kod girdiniz, tekrar deneyiniz.</i> Smart TV’nizde beliren kodu giriniz.</p><!-- tvreflect_info -->
+						<div class="clear"></div>
 
-					<div class="clear"></div>
+						<div class="tvreflect_buttons" id="b1">
+							<a href="javascript:;" onclick="stepOne()">SONRAKİ ADIM</a>
+						</div><!-- tvreflect_buttons -->
 
-					<div class="tvreflect_buttons" id="b1">
-						<a href="javascript:;" onclick="stepOne()">SONRAKİ ADIM</a>
-					</div><!-- tvreflect_buttons -->
+						<div class="clear"></div>
 
-					<div class="clear"></div>
+						<div class="tvreflect_buttons hide" id="b2">
+							<a href="javascript:;" onclick="stepTwo()" class="tvreflect_buttons_half2 danger">DOĞRULA</a>
+							<a href="javascript:;" onclick="stepZore()" class="tvreflect_buttons_half1">ÖNCEKİ ADIM</a>
+						</div><!-- tvreflect_buttons -->
 
-					<div class="tvreflect_buttons hide" id="b2">
-						<a href="javascript:;" onclick="stepTwo()" class="tvreflect_buttons_half2 danger">DOĞRULA</a>
-						<a href="javascript:;" onclick="stepZore()" class="tvreflect_buttons_half1">ÖNCEKİ ADIM</a>
-					</div><!-- tvreflect_buttons -->
+						<div class="clear"></div>
 
-					<div class="clear"></div>
+						<div class="tvreflect_buttons hide" id="b3">
+							<a href="javascript:;" onclick="tvreRlect();tvreRlectIconActive();" class="tvreflect_buttons_half2 success">TAMAM</a>
+							<a href="javascript:;" onclick="stepZore()" class="tvreflect_buttons_half1">İPTAL ET</a>
+						</div><!-- tvreflect_buttons -->
 
-					<div class="tvreflect_buttons hide" id="b3">
-						<a href="javascript:;" onclick="tvreRlect();tvreRlectIconActive();" class="tvreflect_buttons_half2 success">TAMAM</a>
-						<a href="javascript:;" onclick="stepZore()" class="tvreflect_buttons_half1">İPTAL ET</a>
-					</div><!-- tvreflect_buttons -->
+				        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+				        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
 
-			        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-			        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+						<script>
 
-					<script>
+							function tvreRlect(){
+								$(".tvreflect_tooltip").toggle();
+							}
 
-						function tvreRlect(){
-							$(".tvreflect_tooltip").toggle();
-						}
-
-						function tvreRlectIcon(){
-							$(".tvreflect_icon").toggleClass("active");
-						}
-						function tvreRlectIconActive(){
-							$(".tvreflect_icon").addClass("active");
-						}
-						function stepZore(){
-							$(".tvreflect_icons").removeClass("active");
-							$(".tvreflect_icons").removeClass("success");
-							$(".tvreflect_icon_success").removeClass("spaceInUp");
-							$(b1).removeClass("hide");
-							$(b2).addClass("hide");
-							$(b3).addClass("hide");
-							$(p1).removeClass("hide");
-							$(p2).addClass("hide");
-							$(p3).addClass("hide");
-							$(p4).addClass("hide");
-						}
-						function stepOne(){
-							stepZore();
-							$(".tvreflect_icons").addClass("active");
-							$(p1).addClass("hide");
-							$(p2).removeClass("hide");
-							$(b1).addClass("hide");
-							$(b2).removeClass("hide");
-						}
-						function stepError(){
-							stepOne();
-							$(p2).addClass("hide");
-							$(p4).removeClass("hide");
-							$(".tvreflect_icons").addClass("shake").delay(1000).queue(function(){
-							    $(".tvreflect_icons").removeClass("shake").dequeue();
-							});
-						}
-						function stepTwo(){
-							stepOne();
-							$(".tvreflect_icons").toggleClass("success");
-							$(p2).addClass("hide");
-							$(p3).removeClass("hide");
-							$(b2).addClass("hide");
-							$(b3).removeClass("hide");
-							$(".tvreflect_icon_success").addClass("spaceInUp");
-							
-						}
-					</script>
-
-
-
-
-
+							function tvreRlectIcon(){
+								$(".tvreflect_icon").toggleClass("active");
+							}
+							function tvreRlectIconActive(){
+								$(".tvreflect_icon").addClass("active");
+							}
+							function stepZore(){
+								$(".tvreflect_icons").removeClass("active");
+								$(".tvreflect_icons").removeClass("success");
+								$(".tvreflect_icon_success").removeClass("spaceInUp");
+								$(b1).removeClass("hide");
+								$(b2).addClass("hide");
+								$(b3).addClass("hide");
+								$(p1).removeClass("hide");
+								$(p2).addClass("hide");
+								$(p3).addClass("hide");
+								$(p4).addClass("hide");
+							}
+							function stepOne(){
+								stepZore();
+								$(".tvreflect_icons").addClass("active");
+								$(p1).addClass("hide");
+								$(p2).removeClass("hide");
+								$(b1).addClass("hide");
+								$(b2).removeClass("hide");
+							}
+							function stepError(){
+								stepOne();
+								$(p2).addClass("hide");
+								$(p4).removeClass("hide");
+								$(".tvreflect_icons").addClass("shake").delay(1000).queue(function(){
+								    $(".tvreflect_icons").removeClass("shake").dequeue();
+								});
+							}
+							function stepTwo(){
+								stepOne();
+								$(".tvreflect_icons").toggleClass("success");
+								$(p2).addClass("hide");
+								$(p3).removeClass("hide");
+								$(b2).addClass("hide");
+								$(b3).removeClass("hide");
+								$(".tvreflect_icon_success").addClass("spaceInUp");
+								
+							}
+						</script>
+						<div class="clear"></div>
 					</div><!-- tooltip_content -->
+					<div class="clear"></div>
 				</div><!-- /tooltip_wrapper -->
 				<div class="clear"></div>
 			</div><!-- tooltip -->
