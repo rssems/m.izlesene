826358cd2784da43d445657c84db5749d2c90b0f
Rıza Selçuk Saydam <rss.ems@gmail.com>
2015-02-02 17:10:03 +0200

    playlists cat page

---
+++ playlist_page.php
@@ -0,0 +1,135 @@
+<?php include "common/meta.php"; ?>
+<?php include "common/header.php"; ?>
+<?php include "common/nav.php"; ?>
+<?php include "common/search.php"; ?>
+
+<link rel="stylesheet" href="css/playlist.css">
+
+<section class="other_pages">
+	<h1>Müzik Listeleri</h1>
+</section>
+<section class="playlist_group">
+	<ul data-id="genre_elements">
+	<li data-id="genre_1"> 
+					<a onclick="track_event('playlist_discovery','genre_1')" title="Türkçe Pop Müzik Listeleri" href="/muzik-listeleri/turkce-pop" style="background-image: url('http://c6.imgiz.com/images/playlist/genre_1.jpg');" alt="Türkçe Pop Müzik Listeleri">
+						<span class="bg"></span><h4>Türkçe Pop</h4> 
+					</a>  
+				</li><li data-id="genre_11"> 
+					<a onclick="track_event('playlist_discovery','genre_12')" title="Elektronik Müzik Listeleri" href="/muzik-listeleri/elektronik" style="background-image: url('http://c6.imgiz.com/images/playlist/genre_11.jpg');" alt="Elektronik Müzik Listeleri">
+						<span class="bg"></span><h4>Elektronik</h4> 
+					</a>  
+				</li><li data-id="genre_7"> 
+					<a onclick="track_event('playlist_discovery','genre_10')" title="Yabancı Pop Müzik Listeleri" href="/muzik-listeleri/yabanci-pop" style="background-image: url('http://c6.imgiz.com/images/playlist/genre_7.jpg');" alt="Yabancı Pop Müzik Listeleri">
+						<span class="bg"></span><h4>Yabancı Pop</h4> 
+					</a>  
+				</li><li data-id="genre_2"> 
+					<a onclick="track_event('playlist_discovery','genre_4')" title="Türkçe Rock Müzik Listeleri" href="/muzik-listeleri/turkce-rock" style="background-image: url('http://c6.imgiz.com/images/playlist/genre_2.jpg');" alt="Türkçe Rock Müzik Listeleri">
+						<span class="bg"></span><h4>Türkçe Rock</h4> 
+					</a>  
+				</li>
+						  
+				<li data-id="genre_6"> 
+					<a onclick="track_event('playlist_discovery','genre_3')" title="Türk Halk Müziği Müzik Listeleri" href="/muzik-listeleri/turk-halk-muzigi" style="background-image: url('http://c6.imgiz.com/images/playlist/genre_6.jpg');" alt="Türk Halk Müziği Müzik Listeleri">
+						<span class="bg"></span><h4>Türk Halk Müziği</h4> 
+					</a>  
+				</li>
+
+							  
+				<li data-id="genre_5"> 
+					<a onclick="track_event('playlist_discovery','genre_6')" title="Türk Sanat Müziği Müzik Listeleri" href="/muzik-listeleri/turk-sanat-muzigi" style="background-image: url('http://c6.imgiz.com/images/playlist/genre_5.jpg');" alt="Türk Sanat Müziği Müzik Listeleri">
+						<span class="bg"></span><h4>Türk Sanat Müziği</h4> 
+					</a>  
+				</li>
+							  
+				<li data-id="genre_18"> 
+					<a onclick="track_event('playlist_discovery','genre_7')" title="Oyun Havaları Müzik Listeleri" href="/muzik-listeleri/oyun-havalari" style="background-image: url('http://c6.imgiz.com/images/playlist/genre_18.jpg');" alt="Oyun Havaları Müzik Listeleri">
+						<span class="bg"></span><h4>Oyun Havaları</h4> 
+					</a>  
+				</li>
+						
+				<li data-id="genre_12"> 
+					<a onclick="track_event('playlist_discovery','genre_5')" title="Arabesk Rap Müzik Listeleri" href="/muzik-listeleri/arabesk-rap" style="background-image: url('http://c6.imgiz.com/images/playlist/genre_12.jpg');" alt="Arabesk Rap Müzik Listeleri">
+						<span class="bg"></span><h4>Arabesk Rap</h4> 
+					</a>  
+				</li>		  
+				<li data-id="genre_15"> 
+					<a onclick="track_event('playlist_discovery','genre_8')" title="Oryantal Müzik Listeleri" href="/muzik-listeleri/oryantal" style="background-image: url('http://c6.imgiz.com/images/playlist/genre_15.jpg');" alt="Oryantal Müzik Listeleri">
+						<span class="bg"></span><h4>Oryantal</h4> 
+					</a>  
+				</li>
+							  
+				<li data-id="genre_3"> 
+					<a onclick="track_event('playlist_discovery','genre_9')" title="Türkçe Rap Müzik Listeleri" href="/muzik-listeleri/turkce-rap" style="background-image: url('http://c6.imgiz.com/images/playlist/genre_3.jpg');" alt="Türkçe Rap Müzik Listeleri">
+						<span class="bg"></span><h4>Türkçe Rap</h4> 
+					</a>  
+				</li>
+							  
+				
+							  
+				<li data-id="genre_8"> 
+					<a onclick="track_event('playlist_discovery','genre_11')" title="Yabancı Rock Müzik Listeleri" href="/muzik-listeleri/yabanci-rock" style="background-image: url('http://c6.imgiz.com/images/playlist/genre_8.jpg');" alt="Yabancı Rock Müzik Listeleri">
+						<span class="bg"></span><h4>Yabancı Rock</h4> 
+					</a>  
+				</li>
+							  
+				
+							  
+				<li data-id="genre_9"> 
+					<a onclick="track_event('playlist_discovery','genre_13')" title="R&amp;B Müzik Listeleri" href="/muzik-listeleri/rb" style="background-image: url('http://c6.imgiz.com/images/playlist/genre_9.jpg');" alt="R&amp;B Müzik Listeleri">
+						<span class="bg"></span><h4>R&amp;B</h4> 
+					</a>  
+				</li>
+							  
+				<li data-id="genre_17"> 
+					<a onclick="track_event('playlist_discovery','genre_14')" title="Klasik Müzik Müzik Listeleri" href="/muzik-listeleri/klasik-muzik" style="background-image: url('http://c6.imgiz.com/images/playlist/genre_17.jpg');" alt="Klasik Müzik Müzik Listeleri">
+						<span class="bg"></span><h4>Klasik Müzik</h4> 
+					</a>  
+				</li>
+							  
+				<li data-id="genre_10"> 
+					<a onclick="track_event('playlist_discovery','genre_15')" title="Caz Müzik Listeleri" href="/muzik-listeleri/caz" style="background-image: url('http://c6.imgiz.com/images/playlist/genre_10.jpg');" alt="Caz Müzik Listeleri">
+						<span class="bg"></span><h4>Caz</h4> 
+					</a>  
+				</li>
+							  
+				<li data-id="genre_16"> 
+					<a onclick="track_event('playlist_discovery','genre_16')" title="Yerli Dj Müzik Listeleri" href="/muzik-listeleri/yerli-dj" style="background-image: url('http://c6.imgiz.com/images/playlist/genre_16.jpg');" alt="Yerli Dj Müzik Listeleri">
+						<span class="bg"></span><h4>Yerli Dj</h4> 
+					</a>  
+				</li>
+							  
+				<li data-id="genre_13"> 
+					<a onclick="track_event('playlist_discovery','genre_17')" title="Enstrümantal Müzik Listeleri" href="/muzik-listeleri/enstrumantal" style="background-image: url('http://c6.imgiz.com/images/playlist/genre_13.jpg');" alt="Enstrümantal Müzik Listeleri">
+						<span class="bg"></span><h4>Enstrümantal</h4> 
+					</a>  
+				</li>
+							  
+				<li data-id="genre_14"> 
+					<a onclick="track_event('playlist_discovery','genre_18')" title="Şiir Müzik Listeleri" href="/muzik-listeleri/siir" style="background-image: url('http://c6.imgiz.com/images/playlist/genre_14.jpg');" alt="Şiir Müzik Listeleri">
+						<span class="bg"></span><h4>Şiir</h4> 
+					</a>  
+				</li>
+							  
+				<li data-id="genre_19"> 
+					<a onclick="track_event('playlist_discovery','genre_19')" title="Özgün Müzik Listeleri" href="/muzik-listeleri/ozgun-muzik" style="background-image: url('http://c6.imgiz.com/images/playlist/genre_19.jpg');" alt="Özgün Müzik Listeleri">
+						<span class="bg"></span><h4>Özgün Müzik</h4> 
+					</a>  
+				</li>
+							  
+				<li data-id="genre_20"> 
+					<a onclick="track_event('playlist_discovery','genre_20')" title="Etnik Müzik Listeleri" href="/muzik-listeleri/etnik-muzik" style="background-image: url('http://c6.imgiz.com/images/playlist/genre_20.jpg');" alt="Etnik Müzik Listeleri">
+						<span class="bg"></span><h4>Etnik Müzik</h4> 
+					</a>  
+				</li>
+							  
+				<li data-id="genre_21"> 
+					<a onclick="track_event('playlist_discovery','genre_21')" title="İlahi Listeleri" href="/muzik-listeleri/ilahiler" style="background-image: url('http://c6.imgiz.com/images/playlist/genre_21.jpg');" alt="İlahi Listeleri">
+						<span class="bg"></span><h4>İlahiler</h4> 
+					</a>  
+				</li>
+			 
+		</ul>
+</section>
+
+<?php include "common/ad_320_50.php"; ?>
+<?php include "common/footer.php"; ?>
